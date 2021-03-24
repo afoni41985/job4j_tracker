453d51a4969d11c7a9e7406e4b0057a70e941e02
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-03-24 21:30:15 +0300

    1.5. Поля объекта [#484745]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    Error() {
+    }
+
+    Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("active: " + active);
+        System.out.println("status: " + status);
+        System.out.println("message: " + message);
+        System.out.println(" ");
+    }
+
+    public static void main(String[] args) {
+        Error operation = new Error();
+        Error operation2 = new Error(true, 1, "ок");
+        operation.printInfo();
+        operation2.printInfo();
+
+    }
+}
