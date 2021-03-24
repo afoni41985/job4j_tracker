268509ebdeff019255881d0019f9cf5906b4cb3e
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-03-24 19:24:21 +0300

    1.5. Поля объекта [#484745]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -21,11 +21,12 @@ public class Cat {
     public static void main(String[] args) {
         System.out.println("There are gav's food.");
         Cat gav = new Cat();
-        gav.giveNick("stalin");
+        gav.giveNick("Stalin");
         gav.eat("kotleta");
         gav.show();
         System.out.println("There are black's food.");
         Cat black = new Cat();
+        black.giveNick("Lenin");
         black.eat("fish");
         black.show();
     }
