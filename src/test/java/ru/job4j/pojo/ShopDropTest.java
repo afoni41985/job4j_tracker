e60fe5cce374fc739c8b98fde921e0d46dedeb88
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-03-30 23:13:27 +0300

    3.2. Удаление моделей из массива. [#484771]

--- src/test/java/ru/job4j/pojo/ShopDropTest.java
+++ src/test/java/ru/job4j/pojo/ShopDropTest.java
@@ -1,10 +1,9 @@
 package ru.job4j.pojo;
 
-import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.nullValue;
 
 public class ShopDropTest {
 
@@ -12,19 +11,25 @@ public class ShopDropTest {
     public void whenDropFirst() {
         Product[] products = new Product[2];
         products[0] = new Product("Milk", 10);
-        products[1] = new Product("Bread", 4);
-        Product[] rsl = ShopDrop.leftShift(products, 0);
-        assertThat(rsl[0].getName(), is("Bread"));
-        assertThat(rsl[1], is(nullValue()));
+        products[1] = new Product("Bread", 10);
+        Product[] rsl = ShopDrop.leftShift(products, 1);
+        assertThat(rsl[0].getName(), is("Milk"));
+        assertThat(rsl[1], nullValue());
     }
 
     @Test
-    public void whenDropLast() {
-        Product[] products = new Product[2];
+    public void whenDropTwo() {
+        Product[] products = new Product[5];
         products[0] = new Product("Milk", 10);
-        products[1] = new Product("Bread", 4);
-        Product[] rsl = ShopDrop.leftShift(products, 1);
+        products[1] = new Product("Bread", 20);
+        products[2] = new Product("Egg", 11);
+        products[3] = new Product("Fish", 13);
+        products[4] = new Product("Meet", 14);
+        Product[] rsl = ShopDrop.leftShift(products, 2);
         assertThat(rsl[0].getName(), is("Milk"));
-        assertThat(rsl[1], is(nullValue()));
+        assertThat(rsl[1].getName(), is("Bread"));
+        assertThat(rsl[2].getName(), is("Fish"));
+        assertThat(rsl[3].getName(), is("Meet"));
+        assertThat(rsl[4], nullValue());
     }
 }
