274ce80a6884c4e48731cd664dccf97387cf528b
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-03-31 07:28:39 +0300

    3.2. Удаление моделей из массива. [#484771]

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -6,6 +6,7 @@ public class ShopDrop {
             products[i] = products[ i + 1];
             products[i + 1] = null;
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
