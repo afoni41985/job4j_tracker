c1c256fb46e290eb50e4c0cf7eb0221590b62271
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-04-06 22:01:22 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#484698]

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman gina = new Freshman();
+        Student number = gina;
+        Object n = gina;
+    }
+}
+/* 3. Ответьте на вопрос к какому типу приведения это относится?
+здесть происходит повышение типа.
+ */
