c4e1727fd2f664d8c604096955248504fe8e973e
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-04-13 23:12:57 +0300

    0.4. Пользовательские исключения. [#484715]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,23 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException {
+        String[] value = {"one", " two", "tree"};
+        try {
+            indexOf(value, "a");
+        } catch (ElementNotFoundException q) {
+            q.printStackTrace();
+        }
+    }
+}
