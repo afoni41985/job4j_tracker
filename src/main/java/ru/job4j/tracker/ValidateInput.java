0a75604da34c7b391d39ffcc7bd9a5d84fa49d53
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-04-18 09:27:59 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#484720]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 public class ValidateInput extends ConsoleInput {
+
     @Override
     public int askInt(String question) {
         boolean invalid = true;
@@ -8,21 +9,6 @@ public class ValidateInput extends ConsoleInput {
         do {
             try {
                 value = super.askInt(question);
-                invalid = false;
-            } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data again.");
-            }
-        } while (invalid);
-        return value;
-    }
-
-    @Override
-    public String askStr(String question) {
-        boolean invalid = true;
-        String value = null;
-        do {
-            try {
-                value = super.askStr(question);
                 invalid = false;
             } catch (NumberFormatException nfe) {
                 System.out.println("Please enter validate data again.");
