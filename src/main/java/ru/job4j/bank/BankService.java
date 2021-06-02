f7a6730f9902d1d06756a951cb1a9c9c807dc7e7
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-06-02 21:30:39 +0300

    3. Банковские переводы [#10038 #163172]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -50,7 +50,8 @@ public class BankService {
         boolean rsl = false;
         Account srcAccount = findByRequisite(srcPassport, srcRequisite);
         Account destAccount = findByRequisite(destPassport, destRequisite);
-        if (srcAccount != null && srcAccount.getBalance() >= amount && destAccount != null) {
+        if (srcAccount != null && destAccount != null
+                && amount <= srcAccount.getBalance()) {
             srcAccount.setBalance(srcAccount.getBalance() - amount);
             destAccount.setBalance(destAccount.getBalance() + amount);
             rsl = true;
