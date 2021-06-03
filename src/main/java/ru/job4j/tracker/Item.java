06f67a3a02879c5e5373311b2af833817fae5190
afoni41985 <79920297+afoni41985@users.noreply.github.com>
2021-06-03 18:46:07 +0300

    0. Сортировка [#10096 #163670]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 
-public class Item {
+public class Item implements Comparable<Item> {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
@@ -43,5 +43,10 @@ public class Item {
     public String toString() {
         return "Item{" + "id=" + id + ", name='"
                 + name + '\'' + ", created=" + created + '}';
+    }
+
+    @Override
+    public int compareTo(Item another) {
+        return Integer.compare(id, another.id);
     }
 }
---
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ItemTest {
+    @Test
+    public void sortByIdUp() {
+        Item item1 = new Item("a", 1);
+        Item item2 = new Item("b", 2);
+        Item item3 = new Item("c", 3);
+        List<Item> rsl = Arrays.asList(item2, item3, item1);
+        List<Item> expected = Arrays.asList(item1, item2, item3);
+        Collections.sort(rsl);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void sortByIdDown() {
+        Item item1 = new Item("a", 1);
+        Item item2 = new Item("b", 2);
+        Item item3 = new Item("c", 3);
+        List<Item> rsl = Arrays.asList(item1, item2, item3);
+        List<Item> expected = Arrays.asList(item3, item2, item1);
+        Collections.sort(rsl, Collections.reverseOrder());
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void sortByNameUp() {
+        Item item1 = new Item("a", 1);
+        Item item2 = new Item("b", 2);
+        Item item3 = new Item("c", 3);
+        List<Item> rsl = Arrays.asList(item2, item1, item3);
+        List<Item> expected = Arrays.asList(item1, item2, item3);
+        Collections.sort(rsl, new SortByName());
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void sortByNameDown() {
+        Item item1 = new Item("a", 1);
+        Item item2 = new Item("b", 2);
+        Item item3 = new Item("c", 3);
+        List<Item> rsl = Arrays.asList(item1, item2, item3);
+        List<Item> expected = Arrays.asList(item3, item2, item1);
+        Collections.sort(rsl, new SortByName().reversed());
+        assertThat(rsl, is(expected));
+    }
+}
