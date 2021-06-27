147b447fba1749b4c6a00dc3bb13d3157053f204
= <=afoni41985@yandex.ru>
2021-06-27 20:23:34 +0300

    6. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/collection/JobSorter.java
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -2,6 +2,7 @@ package ru.job4j.collection;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class JobSorter {
@@ -17,13 +18,17 @@ public class JobSorter {
         Collections.sort(job, new SortByNameJob());
         System.out.println(job);
 
-    List<Job> jobs = Arrays.asList(
-            new Job("Fix bug", 1),
-            new Job("Fix bug", 4),
-            new Job("Fix bug", 2),
-            new Job("X task", 0)
-    );
-        Collections.sort(jobs, new SortByNameDown().thenComparing(new JobDescByPriority()));
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        jobs.sort(new SortByNameDown().thenComparing(new JobDescByPriority()));
         System.out.println(jobs);
+        Comparator<Job> compareName = Comparator.comparing(Job::getName);
+        Comparator<Job> comparePriority = Comparator.comparing(Job::getPriority);
+        Comparator<Job> combine = compareName.thenComparing(comparePriority);
+        jobs.sort(combine);
     }
 }
--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,8 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -16,10 +18,14 @@ public class PhoneDictionary {
      * @return Список подощедщих пользователей.
      */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> byName = x -> x.getName().contains(key);
+        Predicate<Person> bySurname = x -> x.getSurname().contains(key);
+        Predicate<Person> byAddress = x -> x.getAddress().contains(key);
+        Predicate<Person> byPhone = x -> x.getPhone().contains(key);
         ArrayList<Person> result = new ArrayList<>();
+        Predicate<Person> combine = byName.or(bySurname).or(byAddress).or(byPhone);
         for (Person p : persons) {
-            if (p.getName().contains(key) || p.getSurname().contains(key)
-                    || p.getPhone().contains(key) || p.getAddress().contains(key)) {
+            if (combine.test(p)) {
                 result.add(p);
             }
         }
