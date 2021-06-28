ba1c1c23efc40e532159b8ef453aa204ad9a4e7b
= <=afoni41985@yandex.ru>
2021-06-28 23:42:43 +0300

    2. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,20 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Profile {
+    private Address address;
+
+    public Profile(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+}
