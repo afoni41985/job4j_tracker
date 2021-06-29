79084017a6e0c98057db8d37e06deac86bc4fc1e
= <=afoni41985@yandex.ru>
2021-06-29 22:38:34 +0300

    3. Уникальность элементов и сортировка. [#110225 #172823]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -8,8 +8,8 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
+                .sorted(Comparator.comparing(Address::getCity))
                 .collect(Collectors.toList());
     }
 }
