535b9fe33cf65a556b3e3b36366d7e4e49f28154
= <=afoni41985@yandex.ru>
2021-06-29 23:58:41 +0300

    5. Преобразование матрицы чисел в список чисел.

--- src/main/java/ru/job4j/stream/Matrix.java
+++ src/main/java/ru/job4j/stream/Matrix.java
@@ -3,12 +3,12 @@ package ru.job4j.stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Matrix {
-public static List<Integer> tolist(Integer[][] num) {
-    return Arrays.stream(num)
-             .flatMap(Arrays::stream)
-             .collect(Collectors.toList());
-
-}
+    public static List<Integer> tolist(Integer[][] num) {
+        return Stream.of(num)
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
+    }
 }
--- src/test/java/ru/job4j/stream/MatrixTest.java
+++ src/test/java/ru/job4j/stream/MatrixTest.java
@@ -1,10 +1,7 @@
 package ru.job4j.stream;
-
 import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.List;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
