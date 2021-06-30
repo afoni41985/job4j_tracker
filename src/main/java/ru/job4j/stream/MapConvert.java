2b18494d76413dc6ab98341cb9db4f90badf8258
= <=afoni41985@yandex.ru>
2021-06-30 15:10:51 +0300

    3. Уникальность элементов и сортировка. [#110225 #172823]

--- src/main/java/ru/job4j/stream/MapConvert.java
+++ src/main/java/ru/job4j/stream/MapConvert.java
@@ -7,9 +7,9 @@ import java.util.stream.Collectors;
 public class MapConvert {
     public static Map<String, Student> convertList(List<Student> st) {
         return st.stream()
-                .distinct()
                 .collect(Collectors.toMap(
                         Student::getSurname,
-                        s -> s));
+                        s -> s,
+                        (s, e) -> s));
     }
 }
