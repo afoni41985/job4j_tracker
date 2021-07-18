4c8a24e92d4941fa4f4c18dd8d5f9103eff4a5e9
= <=afoni41985@yandex.ru>
2021-07-18 10:30:11 +0300

    2.2. Вывести четные в порядке убывания индексов

---
+++ src/main/java/ru/job4j/exercise/OddPrint.java
@@ -0,0 +1,13 @@
+package ru.job4j.exercise;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class OddPrint {
+    public static void print(int[] array) {
+        Arrays.stream(array)
+                .filter(num -> num % 2 == 0)
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .forEach(System.out::println);
+    }
+}
